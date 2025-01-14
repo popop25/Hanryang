b0268d0eb44ad2ec3a5b4dfe3c6321a7b40240ae
pop <popramel25@gmail.com>
2025-01-14 22:18:48 +0900

    fix: fix docker setup

--- be/config/config.ts
+++ be/config/config.ts
@@ -21,9 +21,9 @@ export default {
     dialect: "mysql" as Dialect,
   },
   production: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "251625",
-    database: `${process.env.DB_DATABASE}_prod` || "playground_prod",
+    username: process.env.DB_USERNAME || "admin",
+    password: process.env.DB_PASSWORD || "1q2w3e4r!",
+    database: process.env.DB_DATABASE || "playground_production",
     host: process.env.DB_HOST || "127.0.0.1",
     port: Number(process.env.DB_PORT) || 3306,
     dialect: "mysql" as Dialect,
