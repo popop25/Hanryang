e2bcec816ab07121881997ac122bc3569e8a03d1
pop <popramel25@gmail.com>
2025-01-14 22:47:38 +0900

    fix: EC2 외부에서 프론트엔드에 접근 허용

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"), // @를 src 폴더로 매핑
     },
   },
+  server: {
+    host: "0.0.0.0", // 외부에서 접속 가능하도록 설정
+    port: 3000, // 사용할 포트 번호
+  },
 });
